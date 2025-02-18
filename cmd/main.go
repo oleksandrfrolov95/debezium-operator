9b5d26b93cd21513d46f844b7fd0319909dee37f
Oleksandr Frolov <o.frolov.devops@gmail.com>
2025-02-18 18:46:24 +0200

    Add webhook registration

--- cmd/main.go
+++ cmd/main.go
@@ -130,6 +130,11 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DebeziumConnector")
 		os.Exit(1)
 	}
+	// Register the webhook for DebeziumConnector.
+	if err := (&apiv1alpha1.DebeziumConnector{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "DebeziumConnector")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
